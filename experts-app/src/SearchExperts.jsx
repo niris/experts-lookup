2038a3ff6da6b239e3759a56c1e98862a25ad4bd
niris <test@github.local.com>
2023-05-15 18:44:23 +0200

    debug frontend deployment

--- experts-app/src/SearchExperts.jsx
+++ experts-app/src/SearchExperts.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./searchExperts.css";
+import "./SearchExperts.css";
 
 function SearchExperts() {
   const [skillOptions, setskillOptions] = useState([]);
