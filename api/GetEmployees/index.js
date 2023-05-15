37581c50c35880a995a6ead57d4af9ef74d2d618
niris <test@github.local.com>
2023-05-15 19:58:16 +0200

    debug azure function CI/CD

--- api/GetEmployees/index.js
+++ api/GetEmployees/index.js
@@ -3,7 +3,8 @@ const db = require("../lib/azure-cosmosdb-mongodb");
 db.init();
 
 module.exports = async function (context, req) {
-  if (!req.body) { // Get all employees
+  if (!req.body) {
+    // Get all employees
     try {
       const employees = await db.searchEmployees({});
       context.res = {
@@ -16,7 +17,8 @@ module.exports = async function (context, req) {
       context.log(`Error finding employees: ${error.message}`);
       throw error;
     }
-  } else { // Find the experts
+  } else {
+    // Find the experts
     const skills = req.body.skills;
     const experts = await findExperts(skills);
     context.res = {
@@ -29,10 +31,12 @@ module.exports = async function (context, req) {
 };
 
 async function findExperts(skills) {
-  console.log(skills)
+  console.log(skills);
   try {
     const regexPatterns = skills.map((skill) => new RegExp(skill, "i"));
-    const res = await db.searchEmployees({ "skills.languages": { $in: regexPatterns } });
+    const res = await db.searchEmployees({
+      "skills.languages": { $in: regexPatterns },
+    });
     return res;
   } catch (error) {
     context.error(`Error generating recipe: ${error.message}`);
